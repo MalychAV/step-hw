cdf95b07df196e74d724c2d73bac154ae8dcd6db
MalychAV <92050663+MalychAV@users.noreply.github.com>
2021-11-14 18:14:11 +0200

    Add files via upload

---
+++ hw1-2(14.11.2021)/js/main.js
@@ -0,0 +1,21 @@
+
+
+let num1 = prompt('Введите первое число');
+
+let num2 = prompt('Введите второе число');
+num1 = Number(num1);
+num2 = Number(num2);
+
+
+let plus = num1 + num2;
+let minus = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+let rem = num1 % num2;
+
+
+console.log(plus);
+console.log(minus);
+console.log(mult);
+console.log(div);
+console.log(rem);
